d7b83c00a08cba77d4182268eea530bc707fc1f4
Your Name <rodsguilherme//@gmail.com>
2019-11-28 09:48:21 -0300

    add upload image

--- admin/src/components/itens.vue
+++ admin/src/components/itens.vue
@@ -1,93 +1,96 @@
 <template >
-      <v-data-table :headers="headers" :items="menu" sort-by="name" class="elevation-1" dark >
-        <template v-slot:top>
-          <v-toolbar flat dark>
-            <v-toolbar-title >Lista de itens</v-toolbar-title>
-            <v-divider class="mx-4" inset vertical></v-divider>
-            <v-spacer></v-spacer>
-            <v-btn color="teal lighten-1" dark class="mb-2" @click="dialog = true" >Novo Item</v-btn>
+  <v-data-table :headers="headers" :items="menu" sort-by="name" class="elevation-1" dark>
+    <template v-slot:top>
+      <v-toolbar flat dark>
+        <v-toolbar-title>Lista de itens</v-toolbar-title>
+        <v-divider class="mx-4" inset vertical></v-divider>
+        <v-spacer></v-spacer>
+        <v-btn color="teal lighten-1" dark class="mb-2" @click="dialog = true">Novo Item</v-btn>
 
-            <v-dialog v-model="dialog" max-width="500px">
-              <v-card>
-                <v-card-title>
-                  <span class="headline">{{ formTitle }}</span>
-                </v-card-title>
+        <v-dialog v-model="dialog" max-width="500px">
+          <v-card>
+            <v-card-title>
+              <span class="headline">{{ formTitle }}</span>
+            </v-card-title>
 
-                <v-card-text>
-                  <v-container>
-                    <v-row>
-                      <v-col cols="12" sm="6" md="4">
-                        <v-text-field required v-model="name" label="Nome"  ></v-text-field>
-                      </v-col>
-                      <v-col cols="12" sm="6" md="4">
-                        <v-text-field v-model="price" label="Preço" required></v-text-field>
-                      </v-col>
-                      <v-col cols="12" sm="6" md="4">
-                        <v-text-field v-model="category" label="Categoria" required></v-text-field>
-                      </v-col>
-                      <v-col cols="12" sm="6" md="4">
-                        <v-text-field v-model="description" label="Descrição" required></v-text-field>
-                      </v-col>
-                      <v-col cols="12" sm="6" md="4">
-                        <v-text-field v-model="image" label="image" required ></v-text-field>
-                      </v-col>
-                    </v-row>
-                  </v-container>
-                </v-card-text>
+            <v-card-text>
+              <v-container>
+                <v-row>
+                  <v-col cols="12" sm="6" md="4">
+                    <v-text-field required v-model="name" label="Nome"></v-text-field>
+                  </v-col>
+                  <v-col cols="12" sm="6" md="4">
+                    <v-text-field v-model="price" label="Preço" required></v-text-field>
+                  </v-col>
+                  <v-col cols="12" sm="6" md="4">
+                    <v-text-field v-model="category" label="Categoria" required></v-text-field>
+                  </v-col>
+                  <v-col cols="12" sm="6" md="4">
+                    <v-text-field v-model="description" label="Descrição" required></v-text-field>
+                  </v-col>
+                  <v-col cols="12" sm="6" md="4">
+                    <v-file-input label="Imagem" accept="image/*" v-model="image"></v-file-input>
 
-                <v-card-actions>
-                  <v-spacer></v-spacer>
-                  <v-btn color="#ff4081" text @click="close">Cancel</v-btn>
-                  <v-btn color="#ff4081" text @click="save">Save</v-btn>
-                </v-card-actions>
-              </v-card>
+                    <!-- <v-text-field v-model="image" label="image" required ></v-text-field> -->
+                  </v-col>
+                </v-row>
+              </v-container>
+            </v-card-text>
 
-            </v-dialog>
-          </v-toolbar>
-        </template>
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-btn color="#ff4081" text @click="close">Cancel</v-btn>
+              <v-btn color="#ff4081" text @click="save">Save</v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-dialog>
+      </v-toolbar>
+    </template>
 
-        <template v-slot:item.action="{ item }">
-          <div class="mx-2">
-            <v-icon small @click="editItem(item)" class="iconsList" color="indigo lighten-4">fas fa-edit</v-icon>
-            <v-icon small @click="deleteItem(item)" class="iconsList" color="indigo lighten-4">fas fa-trash-alt</v-icon>
-          </div> 
-        </template>
+    <template v-slot:item.action="{ item }">
+      <div class="mx-2">
+        <v-icon small @click="editItem(item)" class="iconsList" color="indigo lighten-4">fas fa-edit</v-icon>
+        <v-icon
+          small
+          @click="deleteItem(item)"
+          class="iconsList"
+          color="indigo lighten-4"
+        >fas fa-trash-alt</v-icon>
+      </div>
+    </template>
 
-        <template v-slot:no-data>
-          <v-btn color="primary" @click="initialize">Reset</v-btn>
-        </template>
-      </v-data-table>
+    <template v-slot:no-data>
+      <v-btn color="primary" @click="initialize">Reset</v-btn>
+    </template>
+  </v-data-table>
 </template>
 
 
 <script>
-
 import axios from "axios";
 
 export default {
   data: () => ({
-
     dialog: false,
-    
+
     id: "",
     name: "",
     price: "",
     category: "",
     description: "",
-    image: "",
+    image: [],
+    imageBase64: "",
 
     itemsPerPageOptions: [10, 20, 30],
     itemsPerPage: 10,
 
     headers: [
-
       { text: "id", value: "id", align: "left" },
       { text: "Nome", value: "name" },
       { text: "Preço", value: "price" },
       { text: "Categoria", value: "itemCategory" },
       { text: "Descrição", value: "description" },
       { text: "Actions", value: "action", sortable: false }
-
     ],
 
     menu: [],
@@ -95,54 +98,60 @@ export default {
     editedIndex: -1,
 
     editedItem: {
-
       id: "",
       name: "",
       price: "",
       category: "",
       description: "",
       image: ""
-
     },
 
     defaultItem: {
-
       name: "",
       price: "",
       category: "",
       description: "",
       image: ""
-
     },
 
     saveOrEdit: 0
-
   }),
 
   computed: {
-
     formTitle() {
       return this.editedIndex === -1 ? "Novo Item" : "Alterar Item"; // nao entendi isso aqui
     }
-
   },
 
   watch: {
-
     dialog(val) {
       val || this.close();
-    }
+    },
+    image(val) {
+      // var reader = new FileReader();
+      // reader.readAsDataURL(val);
+      // reader.onload = (evt)=> {
+      //   if (evt.target.readyState == FileReader.DONE) {
+      //     this.imageBase64 = evt.target.result;
+      //   }
+      // };
 
+      var file = val;
+      var reader = new FileReader();
+      reader.onloadend = ()=> {
+        console.log("RESULT", reader.result);
+        this.imageBase64 = reader.res
+
+      };
+      reader.readAsDataURL(file);
+    }
   },
 
   created() {
-
     this.initialize();
-
   },
 
   methods: {
-
     initialize() {
       axios
         .get("http://localhost:3000/menu/")
@@ -155,41 +164,35 @@ export default {
     },
 
     editItem(item) {
-
       this.name = item.name;
       this.price = item.price;
       this.description = item.description;
       this.category = item.itemCategory;
       this.image = item.image;
       this.editedIndex = item.id;
-      
+
       this.dialog = true;
       this.saveOrEdit = 1;
-
     },
 
     deleteItem(item) {
-
       const index = this.menu.indexOf(item);
       //indexOfRetorna o primeiro índice em que o elemento pode ser encontrado no array, retorna -1 caso o mesmo não esteja presente.
-      confirm("Voce tem certeza que deseja apagar este item?") && this.menu.splice(index, 1); 
+      confirm("Voce tem certeza que deseja apagar este item?") &&
+        this.menu.splice(index, 1);
       //Splice Altera o conteúdo de uma lista, adicionando novos elementos enquanto remove elementos antigos.
       axios.delete(`http://localhost:3000/menu/${item.id}`);
-
     },
 
     close() {
-
       this.dialog = false;
       setTimeout(() => {
         this.editedItem = Object.assign({}, this.defaultItem);
         this.editedIndex = -1;
       }, 300);
-
     },
 
     save() {
-
       if (this.saveOrEdit == 0) {
         axios
           .post("http://localhost:3000/menu/", {
@@ -197,7 +200,7 @@ export default {
             price: this.price,
             description: this.description,
             category: this.category,
-            image: this.image
+            image: this.imageBase64
           })
           .then(res => {
             this.dialog = false;
@@ -211,12 +214,11 @@ export default {
       } else {
         axios
           .patch(`http://localhost:3000/menu/${this.editedIndex}`, {
-
             name: this.name,
             price: this.price,
             description: this.description,
             category: this.category,
-            image: this.image
+            image: this.imageBase64
           })
           .then(res => {
             console.log("Item Alterado");
--- api/server.js
+++ api/server.js
@@ -4,8 +4,9 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 
 app.use(cors());
-app.use(bodyParser.urlencoded({ extended: false}))
-app.use(bodyParser.json())
+app.use(bodyParser.json({limit: '50mb'}))
+app.use(bodyParser.urlencoded({ limit: '50mb', extended: true}))
+
 
 //=====================ROTAS=========================
 
